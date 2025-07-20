7cac0732dfc0539e6544f678d31ba24acdb2bd8a
Muhammed Emin Erdağ <156822517+griffinsspike@users.noreply.github.com>
2025-07-21 01:14:19 +0300

    Delete backend directory

--- backend/app/__init__.py
+++
@@ -1,5 +0,0 @@
-def init_db():
-    Base.metadata.create_all(engine)
-
-if __name__ == "__main__":
-    init_db()
--- backend/app/dto/general_test_chat/general_test_chat_history_response_dto.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class GeneralTestChatHistoryResponseDTO(BaseModel):
-    user_id: str
-    session_id: str
-    history: list
